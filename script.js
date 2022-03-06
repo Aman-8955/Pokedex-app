ea47475bc0af5600f27d70486ba14231b43327b3
Aman <amanali7239@gmail.com>
2022-03-06 15:23:28 +0530

    Pokemon stats added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const pokedex=document.getElementById("pokedex");
 const fetchPokemon = () => {
     const promises = []
-    for (let i = 1; i <= 500; i++) {
+    for (let i = 1; i <= 100; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url).then((res) => res.json()));
     }
@@ -10,7 +10,9 @@ const fetchPokemon = () => {
             name: data.name,
             id: data.id,
             image: data.sprites.other.home['front_default'],
-            type: data.types.map(type => type.type.name).join(' ,')
+            type: data.types.map(type => type.type.name).join(' ,'),
+            weight:data.weight,
+            stats:data.stats
         }));
         displayPokemon(pokemon);
     });
@@ -20,11 +22,23 @@ const displayPokemon=(pokemon)=>{
     console.log(pokemon);
     const pokemonHTMLString=pokemon.map(pokeman=>`
     <li class="card ${pokeman.type}">
+    <div class="img">
         <img class="cardImg" src="${pokeman.image}"/>
-        <div class="details">
+    </div>
+    <div class="details">
         <h2 class="cardHead">${pokeman.id}.${pokeman.name}</h2>
-        <p class="cardPara">Type: ${pokeman.type}</p>
-        </div>
+        <p class="cardPara ${pokeman.type}">Type: ${pokeman.type}</p>
+    <hr>
+    <div class="about cardPara">
+    <span class="hp">HP:${pokeman.stats[0].base_stat}</span>
+    <span class="attack">ATTACK:${pokeman.stats[1].base_stat}</span>
+    <span class="defense">DEFENSE:${pokeman.stats[2].base_stat}</span>
+    <span class="special-attack">SPECIAL-ATTACK:${pokeman.stats[3].base_stat}</span>
+    <span class="special-defense">SPECIAL-DEFENSE:${pokeman.stats[4].base_stat}</span>
+    <span class="speed">SPEED:${pokeman.stats[5].base_stat}</span>
+    <span class="weight">WEIGHT:${pokeman.weight}</span>
+    </div>
+    </div>
     </li>`
     ).join("");
     pokedex.innerHTML=pokemonHTMLString;
